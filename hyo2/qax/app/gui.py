250cad22f13ab9f258b4701673c38dd681913107
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-09-25 16:52:05 +1000

    load config files when GUI started

--- hyo2/qax/app/gui.py
+++ hyo2/qax/app/gui.py
@@ -7,7 +7,7 @@ import logging
 from hyo2.abc.app.app_style import AppStyle
 from hyo2.abc.lib.logging import set_logging
 from hyo2.qax.app.mainwin import MainWin
-
+from hyo2.qax.lib.config import QaxConfig
 
 logger = logging.getLogger(__name__)
 set_logging(ns_list=["hyo2.qax", ])
@@ -35,6 +35,9 @@ def gui(dev_mode=False):
     sys.argv.append("--disable-web-security")  # temporary fix for CORS warning (QTBUG-70228)
     app = QtWidgets.QApplication(sys.argv)
     app.setStyleSheet(AppStyle.load_stylesheet())
+
+    config = QaxConfig()
+    config.load()
 
     main_win = MainWin()
     sys.excepthook = main_win.exception_hook  # install the exception hook
