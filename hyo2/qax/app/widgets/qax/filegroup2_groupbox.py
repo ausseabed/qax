3c2549dfa45ca830a289b7deac14fa5eaedf217f
Lachlan Hurst <lachlan.hurst@gmail.com>
2023-11-23 17:24:18 +1100

    WIP: reworking the input file specification UI. Basic tabel view of inputs presented, missing much functionality and integration into the rest of QAX

---
+++ hyo2/qax/app/widgets/qax/filegroup2_groupbox.py
@@ -0,0 +1,231 @@
+import logging
+import os
+
+from typing import Optional
+from PySide2 import QtCore, QtGui
+from PySide2.QtWidgets import QSizePolicy, QComboBox, QTableWidget, QGroupBox, QVBoxLayout, QHBoxLayout, QTableWidgetItem, QPushButton, QFileDialog, QHeaderView
+
+from hyo2.qax.app import qta
+from hyo2.qax.app.gui_settings import GuiSettings
+from hyo2.qax.lib.plugin import QaxFileGroup
+from hyo2.qax.lib.plugin_service import PluginService
+
+from ausseabed.qajson.model import QajsonRoot
+
+
+logger = logging.getLogger(__name__)
+
+class GroupRow:
+
+    def __init__(self, filename: str, dataset: str, file_type: str) -> None:
+        self.filename = filename
+        self._dataset = dataset
+        self._user_set_dataset = False
+        self._file_type = file_type
+        self._user_set_file_type = False
+
+    @property
+    def filename_short(self) -> str:
+        return os.path.basename(self.filename)
+
+    @property
+    def dataset(self) -> str:
+        return self._dataset
+
+    @dataset.setter
+    def dataset(self, val: str) -> None:
+        self._user_set_dataset = True
+        self._dataset = val
+
+    @property
+    def file_type(self) -> str:
+        return self._file_type
+
+    @file_type.setter
+    def file_type(self, val: str) -> None:
+        self._user_set_file_type = True
+        self._file_type = val
+
+    def __str__(self) -> str:
+        return f"{self.filename_short} {self.dataset} {self.file_type}"
+
+
+class FileGroup2GroupBox(QGroupBox):
+
+    filenames_added = QtCore.Signal(list[str])
+
+    def __init__(self, parent_win, prj):
+        QGroupBox.__init__(self, "Survey Products")
+        self.prj = prj
+        self.parent_win = parent_win
+
+        self.plugin_service:Optional[PluginService] = None
+        self.no_checks_selected_layout = None
+
+        self.rows:list[GroupRow] = []
+        self.available_datasets: list[str] = []
+        self.available_datasets.append("default")
+        self.available_types: list[str] = []
+
+
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+
+        # colors = [("Red", "#FF0000"),
+        #   ("Green", "#00FF00"),
+        #   ("Blue", "#0000FF"),
+        #   ("Black", "#000000"),
+        #   ("White", "#FFFFFF"),
+        #   ("Electric Green", "#41CD52"),
+        #   ("Dark Blue", "#222840"),
+        #   ("Yellow", "#F9E56d")
+        # ]
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(["Filename", "Dataset", "Type"])
+        self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeToContents)
+        self.table.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeToContents)
+
+
+
+        main_layout.addWidget(self.table)
+
+        button_layout = QHBoxLayout()
+        button_layout.addStretch()
+        self.add_file_button = QPushButton()
+        button_layout.addWidget(self.add_file_button)
+        self.add_file_button.setText("Add File")
+        self.add_file_button.setIcon(qta.icon('fa.folder-open'))
+        self.add_file_button.setToolTip("Add survey product files")
+        self.add_file_button.clicked.connect(self._click_add_file)
+        self.clear_file_button = QPushButton()
+        button_layout.addWidget(self.clear_file_button)
+        self.clear_file_button.setText("Clear Files")
+        self.clear_file_button.setToolTip("Clear all survey product files")
+        self.clear_file_button.clicked.connect(self._click_clear_files)
+
+        main_layout.addLayout(button_layout)
+
+    def __update_table(self):
+        self.table.setRowCount(len(self.rows))
+        for i, row in enumerate(self.rows):
+            item_filename = QTableWidgetItem(row.filename_short)
+            item_filename.setToolTip(row.filename)
+            self.table.setItem(i, 0, item_filename)
+
+            item_dataset = QComboBox()
+            item_dataset.setSizePolicy(
+                QSizePolicy.MinimumExpanding, QSizePolicy.Preferred)
+            for ds in self.available_datasets:
+                item_dataset.addItem(ds)
+            item_dataset.setCurrentIndex(self.available_datasets.index(row.dataset))
+            # item_dataset.currentIndexChanged.connect(lambda: self._dataset_changed(item_dataset, row))
+            self.table.setCellWidget(i, 1, item_dataset)
+
+            item_type = QComboBox()
+            item_type.setSizePolicy(
+                QSizePolicy.MinimumExpanding, QSizePolicy.Preferred)
+            for t in self.available_types:
+                item_type.addItem(t)
+            item_type.setCurrentIndex(self.available_types.index(row.file_type))
+            item_type.currentIndexChanged.connect(
+                lambda x, row=row: self._file_type_changed(x, row)
+            )
+            self.table.setCellWidget(i, 2, item_type)
+
+    def __add_new_files(self, filenames: list[str]) -> None:
+        for filename in filenames:
+            gr = GroupRow(
+                filename=filename,
+                dataset='default',
+                file_type=self.available_types[0]
+            )
+            self.rows.append(gr)
+
+        self.__update_table()
+
+    def _dataset_changed(self, cb: QComboBox, row:GroupRow):
+        print(cb)
+        print(row)
+
+    def _file_type_changed(self, index: int, row:GroupRow):
+        row.file_type = self.available_types[index]
+        print(row)
+
+    def _click_add_file(self):
+        import_folder_name = "import_folder_all"
+        filters:list[str] = []
+        all_ext:list[str] = []
+        for file_group in self.plugin_service.get_all_file_groups():
+            all_fg_ext = [
+                "*.{}".format(ft.extension)
+                for ft in file_group.file_types
+            ]
+            all_ext.extend(all_fg_ext)
+            all_fg_ext_str = " ".join(all_fg_ext)
+            all_fg_formats = f"{file_group.name} ({all_fg_ext_str})"
+            filters.append(all_fg_formats)
+
+        all_ext_str = " ".join(all_ext)
+        filters.insert(0, f"All Supported Files ({all_ext_str})")
+
+        filters.append("All files (*.*)")
+
+        # ask the file path to the user
+        # noinspection PyCallByClass
+        selections, _ = QFileDialog.getOpenFileNames(
+            self,
+            "Add File",
+            GuiSettings.settings().value(import_folder_name),
+            ";; ".join(filters))
+        if len(selections) == 0:
+            logger.debug('adding raw: aborted')
+            return
+
+        last_open_folder = os.path.dirname(selections[0])
+        if os.path.exists(last_open_folder):
+            GuiSettings.settings().setValue(
+                import_folder_name, last_open_folder)
+
+        new_selected_files = [
+            os.path.abspath(selection).replace("\\", "/")
+            for selection in selections
+        ]
+
+        self.__add_new_files(new_selected_files)
+        # self.filenames_added.emit(new_selected_files)
+
+
+    def _click_clear_files(self):
+        print("files clear")
+
+
+    def get_rgb_from_hex(self, code):
+        code_hex = code.replace("#", "")
+        rgb = tuple(int(code_hex[i:i+2], 16) for i in (0, 2, 4))
+        
+        return QtGui.QColor.fromRgb(rgb[0], rgb[1], rgb[2])
+
+    def set_plugin_service(self, ps: PluginService):
+        self.plugin_service = ps
+        self.available_types = self.plugin_service.get_all_file_group_names()
+        self.__update_table()
+
+    # def update_file_groups(
+    #         self, file_groups: list[QaxFileGroup]
+    #     ) -> None:
+    #     """ Update what file groups should be open'able in this file
+    #     selection widget
+    #     """
+    #     self.file_groups = file_groups
+    #     self.available_types = [fg.name for fg in self.file_groups]
+    #     self.__update_table()
+
+
+    def update_ui(self, qajson: QajsonRoot) -> None:
+        pass
+
+
+
---
+++ hyo2/qax/lib/plugin_service.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from hyo2.qax.lib.plugin import QaxPlugins, QaxCheckToolPlugin, QaxFileGroup
+
+"""
+Offers plugin related capability as a service that can be injected into
+other classes
+"""
+class PluginService:
+
+    def __init__(self, plugins: list[QaxCheckToolPlugin]) -> None:
+        self.plugins = plugins
+
+    def get_all_file_groups(self) -> list[QaxFileGroup]:
+        all:list[QaxFileGroup] = []
+        for plugin in self.plugins:
+            for file_group in plugin.get_file_groups():
+                all.append(file_group)
+        return all
+
+    def get_all_file_group_names(self) -> list[str]:
+        all = self.get_all_file_groups()
+        all_names = [fg.name for fg in all]
+        all_names.append("Unknown")
+        # remove duplicates, and sort
+        all_names = sorted(list(set(all_names)))
+        return all_names
+
+    def identify_file_group(self, filename: str) -> str:
+        """
+        Identifies the file group this file belongs to based on
+        its name (extension). If no matching filegroup is found
+        then 'Unknown' is returned
+        """
+        for plugin in self.plugins:
+            for file_group in plugin.get_file_groups():
+                ft = file_group.matching_file_type(Path(filename))
+                if ft is not None:
+                    return file_group.name
+        
+        return 'Unknown'
+
