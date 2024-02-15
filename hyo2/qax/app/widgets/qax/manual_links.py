161029a46b350158488fa192ad1f4aa33135397c
Lachlan Hurst <lachlan.hurst@gmail.com>
2024-02-15 13:52:44 +1100

    missed updated manual link

--- hyo2/qax/app/widgets/qax/manual_links.py
+++ hyo2/qax/app/widgets/qax/manual_links.py
@@ -2,3 +2,4 @@
 INDEX = 'index.html'
 INTERFACE_PROFILE = 'user_manual_interface_inputs.html#profile'
 INTERFACE_STANDARD = 'user_manual_interface_inputs.html#standard'
+INTERFACE_CHECK_TOOL_SELECTION = 'user_manual_interface_inputs.html#check-tools'
